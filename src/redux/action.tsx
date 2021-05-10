c72f8f44f562f0047ec8dc844b3bc757011455ce
Aslanov Rustam <aslanovrg@gmail.com>
2021-05-10 15:18:49 +0300

    upd part of store

--- src/redux/action.tsx
+++ src/redux/action.tsx
@@ -1,9 +1,30 @@
 import { createAction } from "@reduxjs/toolkit";
 
-const filterItem = createAction("filter");
+interface IFilterItem {
+  type: string;
+  payload: string;
+}
 
-const checkedItemsForFilter = createAction("filter_checkedItemsForFilter");
+// const filterItem = createAction<string>("filter");
+const filterItem = (text: string): IFilterItem => ({
+  type: "filter",
+  payload: text,
+});
 
-const unCheckedItemsForFilter = createAction("filter_unCheckedItemsForFilter");
+const checkedItemsForFilter = createAction<string>(
+  "filter_checkedItemsForFilter"
+);
+// interface IFilterChecked {
+//   type: string;
+//   payload: boolean;
+// }
+// const checkedItemsForFilter = (text: boolean): IFilterChecked => ({
+//   type: "filter_checkedItemsForFilter",
+//   payload: text,
+// });
+
+const unCheckedItemsForFilter = createAction<string>(
+  "filter_unCheckedItemsForFilter"
+);
 
 export default { filterItem, checkedItemsForFilter, unCheckedItemsForFilter };
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -7,20 +7,57 @@ import { createReducer } from "@reduxjs/toolkit";
 const itemsReducer = (state = db, _action: any) => {
   return state;
 };
-const filterReducer = createReducer("", {
-  [actions.filterItem]: (_state, action) => {
-    return action.payload;
-  },
-});
+// const filterReducer = createReducer<string>("", {
+//   [actions.filterItem]: (_state, action) => {
+//     return action.payload;
+//   },
+// });
+const initialFilter: string = "";
+interface IFilterItem {
+  type: string;
+  payload: string;
+}
+const filterReducer = (state: Array<Object>, action: IFilterItem) => {
+  switch (action.type) {
+    case "filter":
+      return action.payload;
+    default:
+      return initialFilter;
+  }
+};
 
-const checkedItemsForFilterReducer = createReducer([], {
-  [actions.checkedItemsForFilter]: (state, { payload }) => {
-    return [...state, payload];
-  },
-  [actions.unCheckedItemsForFilter]: (state, { payload }) => {
-    return state.filter((item) => item !== payload);
-  },
-});
+interface ICheckedFilterItem {
+  type: string;
+  payload: string[];
+}
+interface IFilterItemChecked {
+  payload: string;
+}
+const checkedItemsForFilterReducer = (
+  state: Array<Object>,
+  action: ICheckedFilterItem
+) => {
+  switch (action.type) {
+    case "filter_checkedItemsForFilter":
+      return (state: string[], { payload }: IFilterItemChecked) => {
+        return [...state, payload];
+      };
+    case "filter_unCheckedItemsForFilter":
+      return (state: string[], { payload }: IFilterItemChecked) => {
+        return state.filter((item) => item !== payload);
+      };
+    default:
+      return initialFilter;
+  }
+};
+// const checkedItemsForFilterReducer = createReducer([], {
+//   [actions.checkedItemsForFilter]: (state, { payload }) => {
+//     return [...state, payload];
+//   },
+//   [actions.unCheckedItemsForFilter]: (state, { payload }) => {
+//     return state.filter((item) => item !== payload);
+//   },
+// });
 
 const rootReducer = combineReducers({
   items: itemsReducer,
